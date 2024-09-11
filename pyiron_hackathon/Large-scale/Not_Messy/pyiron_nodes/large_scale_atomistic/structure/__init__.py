c604911fb9b7996c24d9f74ee212a15031cbfce0
Tarakeshwar Lakshmipathy <87063936+Tara-Lakshmipathy@users.noreply.github.com>
2024-09-11 11:06:21 +0200

    Update __init__.py

--- pyiron_hackathon/Large-scale/Not_Messy/pyiron_nodes/large_scale_atomistic/structure/__init__.py
+++ pyiron_hackathon/Large-scale/Not_Messy/pyiron_nodes/large_scale_atomistic/structure/__init__.py
@@ -1,5 +1,5 @@
 from pyiron_nodes.large_scale_atomistic.structure import (
-    creator_crystal_structure,
+    create_crystal_structure,
     k_controlled_crack,
     visualize_structures,
     boundary_layers
