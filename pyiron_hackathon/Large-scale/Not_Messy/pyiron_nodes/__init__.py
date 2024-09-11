3ab5f635add9583bb6c2a5ba3c5de2b67279f4b4
Tarakeshwar Lakshmipathy <87063936+Tara-Lakshmipathy@users.noreply.github.com>
2024-09-11 10:47:34 +0200

    Create __init__.py

---
+++ pyiron_hackathon/Large-scale/Not_Messy/pyiron_nodes/__init__.py
@@ -0,0 +1,5 @@
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+
+from pyiron_nodes import atomistic, databases, large_scale_atomistic, atomistic_codes, lammps, math, plotting
